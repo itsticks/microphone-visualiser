52d3e29bc644f685fea9e8e4e5b051f7665c9a6c
Pamp <pamp@Pamps-MacBook-Pro.local>
2019-09-02 23:25:08 +0100

    cool

--- main.js
+++ main.js
@@ -24,7 +24,6 @@ ctx = canvas.getContext("2d");
 audioContext = new AudioContext();
 
 navigator.mediaDevices.getUserMedia({audio:{
-//   sampleSize: 8,
     echoCancellation: false,
     autoGainControl:false,
     noiseSuppression:false,
@@ -33,14 +32,13 @@ navigator.mediaDevices.getUserMedia({audio:{
 .then(function(stream) {
     analyser = audioContext.createAnalyser(stream);
     analyser.fftSize = 2048;
-	bufferLength = analyser.frequencyBinCount;
-    dataArray = new Uint8Array(bufferLength);
+
     
     mediaStreamSource = audioContext.createMediaStreamSource(stream);
     volumeMeter = createVolumeMeter(audioContext);
-    mediaStreamSource.connect(volumeMeter);
-    volumeMeter.connect(analyser);
-    analyser.connect(audioContext.destination);
+    mediaStreamSource.connect(analyser);
+    analyser.connect(volumeMeter);
+    volumeMeter.connect(audioContext.destination);
 
     draw();
 })
@@ -48,15 +46,15 @@ navigator.mediaDevices.getUserMedia({audio:{
 }
 
 function draw() {
-
 var randomR = Math.random() * 255;
 var randomB = Math.random() * 255;
 var randomG = Math.random() * 255;
-
-  //  ctx.clearRect(0,0,canvas.width,canvas.height);
+bufferLength = analyser.frequencyBinCount;
+dataArray = new Uint8Array(bufferLength);
+  ctx.clearRect(0,0,canvas.width,canvas.height);
     ctx.lineWidth = 2;
-    ctx.strokeStyle = 'rgb(255, 0, 0)';
-    ctx.globalAlpha = 0.1;
+    ctx.strokeStyle = 'rgb(0, 0, 0)';
+   // ctx.globalAlpha = 0.1;
     ctx.fillStyle = "rgb("+randomR+","+randomB+","+randomG+")";
     ctx.beginPath();
     ctx.arc(canvas.width/2, canvas.height/2, volumeMeter.volume*3000*1.4, 0, 2 * Math.PI);
@@ -66,21 +64,20 @@ var randomG = Math.random() * 255;
     ctx.beginPath();
     var sliceWidth = canvas.width * 1.0 / bufferLength;
     var x = 0;
+    analyser.getByteTimeDomainData(dataArray);
+
     for(var i = 0; i < bufferLength; i++) {
-   
         var v = dataArray[i] / 128.0;
         var y = v * canvas.height/2;
-
         if(i === 0) {
-          ctx.moveTo(x, y);
+          ctx.moveTo(x, y*1);
         } else {
-          ctx.lineTo(x, y);
+          ctx.lineTo(x, y*1);
         }
-
         x += sliceWidth;
       }
       
-      ctx.lineTo(canvas.width, canvas.height/2);
+     // ctx.lineTo(canvas.width/2, canvas.height/2);
       ctx.stroke();
 
     animation = window.requestAnimationFrame( draw );
